5c467280396d217a3d504327696d716541758a3c
Evanilson <Evanilson@pc-evanilson>
2017-03-19 23:25:01 -0300

    Criação dos controladores de Video

--- application/controllers/Video.php
+++ application/controllers/Video.php
@@ -2,10 +2,80 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Video extends CI_Controller {
+class VideoModel extends CI_Controller {
 
     public function index() {
         $this->load->view('teste');
+    }
+
+    public function processaCadastrar() {
+        $titulo = $this->input->post('titulo');
+        $descricao = $this->input->post('descricao');
+        $link = $this->input->post('link');
+        $dataHora = $this->input->post('dataHora');
+
+        $this->load->model('VideoModel');
+        if ($this->VideoModel->insert($titulo, $descricao, $link, $dataHora)) {
+            $response = array('result' => 'true', 'msg' => 'Vídeo cadastrado com sucesso!');
+        } else {
+            $response = array('result' => 'false', 'msg' => 'O vídeo não foi cadastrado!');
+        }
+        echo json_encode($response);
+    }
+
+    public function processaBuscar($id) {
+        $this->load->model('VideoModel');
+        $result = $this->VideoModel->get($id);
+        $response = array();
+        foreach ($result as $row) {
+            $json["id"] = $row->id_vdo;
+            $json["titulo"] = $row->titulo_vdo;
+            $json["descricao"] = $row->descricao_vdo;
+            $json["link"] = $row->link_vdo;
+            $json["dataHora"] = $row->datahora_vdo;
+            array_push($response, $json);
+        }
+        echo json_encode($response);
+    }
+
+    public function processaListar() {
+        $this->load->model('VideoModel');
+        $result = $this->VideoModel->getList();
+        $response = array();
+        foreach ($result as $row) {
+            $json["id"] = $row->id_vdo;
+            $json["titulo"] = $row->titulo_vdo;
+            $json["descricao"] = $row->descricao_vdo;
+            $json["link"] = $row->link_vdo;
+            $json["dataHora"] = $row->datahora_vdo;
+            array_push($response, $json);
+        }
+        echo json_encode($response);
+    }
+
+    public function processaAtualizar($id) {
+        $titulo = $this->input->post('titulo');
+        $descricao = $this->input->post('descricao');
+        $link = $this->input->post('link');
+        $dataHora = $this->input->post('dataHora');
+
+        $this->load->model('VideoModel');
+        if ($this->VideoModel->update($id, $titulo, $descricao, $link, $dataHora)) {
+            $response = array('result' => 'true', 'msg' => 'Vídeo atualizado com sucesso!');
+        } else {
+            $response = array('result' => 'false', 'msg' => 'O vídeo não foi atualizado!');
+        }
+        echo json_encode($response);
+    }
+
+    public function processaRemover($id) {
+        $this->load->model('VideoModel');
+        if ($this->VideoModel->delete($id)) {
+            $response = array('result' => 'true', 'msg' => 'Vídeo removido com sucesso!');
+        } else {
+            $response = array('result' => 'false', 'msg' => 'O vídeo não foi removido!');
+        }
+        echo json_encode($response);
     }
 
 }
--- application/models/VideoModel.php
+++ application/models/VideoModel.php
@@ -1,5 +1,61 @@
 <?php
 
 class VideoModel extends CI_Model {
-    
+
+    private $tbVideo = "video";
+
+    public function insert($titulo, $descricao, $link, $dataHora) {
+        $data = array(
+            'titulo_vdo' => $titulo,
+            'descricao_vdo' => $descricao,
+            'link_vdo' => $link,
+            'datahora_vdo' => $dataHora
+        );
+        if ($this->db->insert($this->tbVideo, $data)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function get($id) {
+        $query = $this->db->select('id_vdo, titulo_vdo, descricao_vdo, link_vdo, datahora_vdo')
+                ->from($this->tbVideo)
+                ->where('id_vdo', $id)
+                ->get();
+        return $query->result();
+    }
+
+    public function getList() {
+        $query = $this->db->select('id_vdo, titulo_vdo, descricao_vdo, link_vdo, datahora_vdo')
+                ->from($this->tbVideo)
+                ->order_by('id_vdo', 'desc')
+                ->get();
+        return $query->result();
+    }
+
+    public function update($id, $titulo, $descricao, $link, $dataHora) {
+        $data = array(
+            'titulo_vdo' => $titulo,
+            'descricao_vdo' => $descricao,
+            'link_vdo' => $link,
+            'datahora_vdo' => $dataHora
+        );
+        $this->db->where('id_vdo', $id);
+        if ($this->db->update($this->tbVideo, $data)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function delete($id) {
+        $this->db->where('id_vdo', $id);
+        if ($this->db->delete($this->tbVideo)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
